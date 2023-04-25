1f43737115f8b222d0625921ceee9626deabdd8d
maxikprogrammist <131183477+maxikprogrammist@users.noreply.github.com>
2023-04-25 19:21:21 +0300

    ничего не сделал

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 
+import pygame
+
+root = pygame.display.set_mode((720, 512))
+pygame.display.set_caption('пинг-понг')
+player_image = 'player.png'
+class GameSprite(sprite.Sprite):
+
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed	x	
+run = True
+while run:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+
+pygame.quit()
