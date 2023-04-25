f090ca629c2e2893135188bf9b41391f98496988
maxikprogrammist <131183477+maxikprogrammist@users.noreply.github.com>
2023-04-25 18:28:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-print('Hello, world')
-print('Hello, world')
-print('Hello, world')
-print('Hello, world')
-print('Hello, world')
-print('Hello, world')
-print('Hello, world')
-print('Hello, world')
-print('Hello, world')
+
